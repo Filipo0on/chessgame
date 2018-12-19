abe368eae7c228da374fe9312332ed5121fb7b15
Filip <landedcosine@live.se>
2018-12-19 13:54:58 +0100

    added grid system and list container

--- src/components/lobby/LobbySeeks.tsx
+++ src/components/lobby/LobbySeeks.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+
+
+
+const Container = styled.div `
+grid-column-start: 2;
+grid-column-end: 3;
+grid-row-start: 2;
+grid-row-end: 3;
+background-color: white;
+border-radius: 10px;
+opacity: 0.85;
+padding: 20px;
+
+
+display: grid;
+grid-template-columns: 80% 10% 10%;
+
+
+
+`
 
 class LobbySeeksComponent extends React.Component<any, any> {
   public render() {
     return (
-      <div>
-        <div>
-            Seeks
-        </div>
-      </div>
+      <Container>
+       <div>Hejsan</div>
+       <div>Hejsan2</div>
+       <div>Hejsan3</div>
+
+      </Container>
     );
   }
 }
--- src/containers/lobby/Lobby.tsx
+++ src/containers/lobby/Lobby.tsx
@@ -1,17 +1,33 @@
 import * as React from 'react';
-
 import LobbyCreateGameComponent from 'src/components/lobby/LobbyCreateGame';
 import LobbyFilterComponent from 'src/components/lobby/LobbyFilter';
 import LobbySeeksComponent from 'src/components/lobby/LobbySeeks';
+import styled from 'styled-components';
+import BGImage from '../../Images/LobbyBG.jpg';
+
+
+const MainContainer = styled.div`  
+background-image: url(${BGImage});
+  margin: 0; 
+  height: 100Vh;
+  display: grid;
+  grid-template-columns: 10% auto 10%;
+  grid-template-rows: 120px auto 120px;
+  
+ 
+` 
+
 
 class LobbyComponent extends React.Component<any, any> {
   public render() {
     return (
-      <div>
-        <LobbySeeksComponent />
-        <LobbyFilterComponent />
-        <LobbyCreateGameComponent />
-      </div>
+        <MainContainer>
+
+          <LobbyFilterComponent />
+          <LobbyCreateGameComponent />
+          <LobbySeeksComponent />
+    
+        </MainContainer>
     );
   }
 }
