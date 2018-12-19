3b7d15fd5dafe8188d802c9b98c3ced0cd2806b4
Linus Plume <plumee_91@hotmail.com>
2018-12-19 12:19:38 +0100

    Input for Game Chat

--- src/components/game/GameChat.tsx
+++ src/components/game/GameChat.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import { GameChatInput } from '../../styles/game/Game';
 
 class GameChatComponent extends React.Component<any, any> {
   public render() {
     return (
       <div>
         GameChatComponent
+        <GameChatInput placeholder={"Vänligen uppträd trevligt i chatten"} />
       </div>
     );
   }
