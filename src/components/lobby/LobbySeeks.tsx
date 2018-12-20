ee2d8598795da1374a1352b60cad4a59aeceae6d
Filip <landedcosine@live.se>
2018-12-20 11:20:43 +0100

    maping games from mockdata

--- src/components/lobby/LobbySeeks.tsx
+++ src/components/lobby/LobbySeeks.tsx
@@ -1,7 +1,7 @@
+// tslint:disable:no-console
 import * as React from 'react';
 import styled from 'styled-components';
-
-
+import {CreatedGames} from '../../store/MockData';
 
 
 const Container = styled.div `
@@ -11,25 +11,80 @@ grid-row-start: 2;
 grid-row-end: 3;
 background-color: white;
 border-radius: 10px;
-opacity: 0.85;
-padding: 20px;
-
-
+opacity: 0.8;
+padding: 50px 40px 0px 40px;
+box-shadow: 5px 5px 30px black, -5px -3px 30px black;
+  
+`
+const ListItemMatch = styled.div `
 display: grid;
-grid-template-columns: 80% 10% 10%;
-
-
+grid-template-columns: 70% 30%;§
+border-bottom: 1px solid black;
+padding: 20px 15px;
+place-items: baseline;
 
 `
+const MatchInfo = styled.div`
 
-class LobbySeeksComponent extends React.Component<any, any> {
+`
+const JoinBTN = styled.button `
+justify-self: end;
+padding: 15px;
+color: white;
+background-color: green; 
+border-radius: 10px;
+border: none;
+
+:focus {
+  outline-width: 0;
+}
+`
+
+interface IStateType  {
+  Player: string;
+}
+
+
+class LobbySeeksComponent extends React.Component<any, IStateType> {
+
+  constructor (props: any) {
+    super(props)
+    this.state = {       
+      
+      Player: "kalle",
+
+    };
+}
+
   public render() {
+    console.log("created games",CreatedGames)
+    const Games = CreatedGames;
+
+const LiatOfGames = Games.map(Game => {
+return (
+  <div key={Game.gameId}>
+    <h1>{Game.creator}</h1>
+  </div>
+);
+});
+   
+
+
     return (
       <Container>
-       <div>Hejsan</div>
-       <div>Hejsan2</div>
-       <div>Hejsan3</div>
-
+          {LiatOfGames}
+          <ListItemMatch>
+            <MatchInfo>{this.state.Player}</MatchInfo>           
+            <JoinBTN>Join Game</JoinBTN>
+          </ListItemMatch>
+          <ListItemMatch>
+            <MatchInfo>Player</MatchInfo>           
+            <JoinBTN>Join Game</JoinBTN>
+          </ListItemMatch>
+          <ListItemMatch>
+            <MatchInfo>Player</MatchInfo>           
+            <JoinBTN>Join Game</JoinBTN>
+          </ListItemMatch>         
       </Container>
     );
   }
--- src/containers/lobby/Lobby.tsx
+++ src/containers/lobby/Lobby.tsx
@@ -3,17 +3,21 @@ import LobbyCreateGameComponent from 'src/components/lobby/LobbyCreateGame';
 import LobbyFilterComponent from 'src/components/lobby/LobbyFilter';
 import LobbySeeksComponent from 'src/components/lobby/LobbySeeks';
 import styled from 'styled-components';
-import BGImage from '../../Images/LobbyBG.jpg';
+import BGImage from '../../dist/images/chess-bg.jpg';
 
 
 const MainContainer = styled.div`  
-background-image: url(${BGImage});
+
+  background-image: url(${BGImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-origin: content-box;
   margin: 0; 
   height: 100Vh;
   display: grid;
   grid-template-columns: 10% auto 10%;
   grid-template-rows: 120px auto 120px;
-  
+ 
  
 ` 
 
