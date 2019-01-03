c7ecc03a8cd36ee1748ee06af5b5cfe8a16e8499
Malin Hanak <malinehanak@gmail.com>
2019-01-03 10:46:26 +0100

    formatted

--- server/index.js
+++ server/index.js
@@ -1,26 +1,75 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const games = [
-    {gameId: "G1", gameType: "Classic", gameTime: "5", gameAddTime: "15", gameStarted: false, creator: "P1", opponent: "P2"},
-    {gameId: "G2", gameType: "Classic", gameTime: "5", gameAddTime: "15", gameStarted: false, creator: "P4", opponent: "P2"},
-    {gameId: "G3", gameType: "Classic", gameTime: "5", gameAddTime: "15", gameStarted: false, creator: "P3", opponent: "P1"},
+const games = [{
+        gameId: "G1",
+        gameType: "Classic",
+        gameTime: "5",
+        gameAddTime: "15",
+        gameStarted: false,
+        creator: ["P1","Anonymous"],
+        opponent: [{
+            userId: "P2",
+            userName: "Anonymous"
+        }]
+    },
+    {
+        gameId: "G2",
+        gameType: "Classic",
+        gameTime: "5",
+        gameAddTime: "15",
+        gameStarted: false,
+        creator: [{
+            userId: "P4",
+            userName: "Anonymous"
+        }],
+        opponent: [{
+            userId: "P2",
+            userName: "Anonymous"
+        }]
+    },
+    {
+        gameId: "G3",
+        gameType: "Classic",
+        gameTime: "5",
+        gameAddTime: "15",
+        gameStarted: false,
+        creator: [{
+            userId: "P3",
+            userName: "Anonymous"
+        }],
+        opponent: [{
+            userId: "P1",
+            userName: "Anonymous"
+        }]
+    },
 ];
 
-const players = [
-    {userId: "P1", userName: "Anonymous"},
-    {userId: "P2", userName: "Anonymous"},
-    {userId: "P3", userName: "Anonymous"},
-    {userId: "P4", userName: "Anonymous"},
+const players = [{
+        userId: "P1",
+        userName: "Anonymous"
+    },
+    {
+        userId: "P2",
+        userName: "Anonymous"
+    },
+    {
+        userId: "P3",
+        userName: "Anonymous"
+    },
+    {
+        userId: "P4",
+        userName: "Anonymous"
+    },
 ]
-const typeDefs = gql`
+const typeDefs = gql `
   type Game {
     gameId: String
     gameType: String
     gameTime: String
     gameAddTime: String
     gameStarted: Boolean
-    creator: String
-    opponent: String
+    creator: Player
+    opponent: Player
   }
 
   type Player {
@@ -35,14 +84,17 @@ const typeDefs = gql`
 `;
 
 const resolvers = {
-  Query: {
-    getGames: () => games,
-    getPlayers: () => players,
-  },
+    Query: {
+        getGames: () => games,
+        getPlayers: () => players,
+    },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen().then(({url}) => {
+    console.log(`🚀  Server ready at ${url}`);
 });
