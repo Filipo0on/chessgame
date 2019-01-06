41c56f357a4811ce655c9cd06fb12257371c5247
filipo0on <landedcosine@live.se>
2019-01-06 20:17:51 +0100

    made changes according to the comments on PR

--- src/components/lobby/LobbyAwaitGame.tsx
+++ src/components/lobby/LobbyAwaitGame.tsx
@@ -3,39 +3,45 @@ import * as React from 'react';
 import styled from 'styled-components';
 import BGImage from '../../dist/images/chess-bg.jpg';
 import {CreatedGames} from '../../store/MockData';
-
 const ServerData = CreatedGames;
 const mockGame = {creator : "Anonymous", creatorColor: "",  opponent : "Anonymous", gameId : 2, gameType : "Classic", gameTime : 5, gameAddTime : 15, gameStarted : false, player1Ready: false, player2Ready: false,}; 
-
-
+interface IGameData {
+    creator: string
+    creatorColor: string
+    gameAddTime: number
+    gameId: number
+    gameStarted: boolean
+    gameTime: number
+    gameType: string | undefined
+    opponent: string
+    player1Ready: boolean
+    player2Ready: boolean
+    }
 interface IStateType  {
   Player1Ready: boolean;
   Player2Ready: boolean;
   IsPlayer1: boolean;
-  gameId: number;
- 
+  gameId: number; 
 }
-class LobbyAwaitGameComponent extends React.Component<any, IStateType> {
-
-  constructor (props: any) {
+interface IPropsType {
+    match: any;
+}
+class LobbyAwaitGameComponent extends React.Component<IPropsType, IStateType> {
+  constructor (props: IPropsType) {
     super(props)
     this.state = {             
       gameId: this.props.match.params.id,     
       IsPlayer1: true,
       Player1Ready: false,
       Player2Ready: false,
-    };
-    
+    }; 
 }
-
-public SetReadyPlayer1 = (GameData : any) => {
+public SetReadyPlayer1 = (gameData : IGameData) => {
     this.setState({Player1Ready: true});
 }
-
-public SetReadyPlayer2 = (GameData : any) => {
+public SetReadyPlayer2 = (gameData : IGameData) => {
     this.setState({Player2Ready: true});
 }
-
 public MakeReady = () => {
    if(this.state.IsPlayer1) {
        console.log('Change player 1 to ready in DB');
@@ -43,19 +49,15 @@ public MakeReady = () => {
        console.log('Change player 2 to ready in DB');
    }
 }
-
 public FindMatchById = () => {
     const findGame = ServerData.find(MatchId => MatchId.gameId === +this.state.gameId);
-
     if(findGame) {
         return findGame;
     }else {
       return mockGame
     }
 }
-
 public render() {  
-
     const gameData = this.FindMatchById();
     const Player1ReadyColor = this.state.Player1Ready ? "green" : "red";
     const Player2ReadyColor = this.state.Player2Ready ? "green" : "red";
@@ -71,9 +73,7 @@ public render() {
     if (gameData.player2Ready === true && this.state.Player2Ready !== true) {        
         this.SetReadyPlayer2(gameData);
      };
-
-    return (
-        
+    return (        
         <Container>
             <Content>
                  <Header>
@@ -106,9 +106,7 @@ public render() {
      );
    } 
 }
-
 export default LobbyAwaitGameComponent;
-
 const Container = styled.div `    
 background-image: url(${BGImage});
 background-repeat: no-repeat;
@@ -120,7 +118,6 @@ display: grid;
 grid-template-columns: 10% auto 10%;
 grid-template-rows: 120px auto 120px;   
 `
-
 const Content = styled.div `
 grid-column-start: 2;
 grid-column-end: 3;
@@ -134,7 +131,6 @@ display: grid;
 grid-template-columns: 5% 50% 35% 10%;
 grid-template-rows: 80px auto 150px;  
 `
-
 const Header = styled.div `
 grid-column-start: 1;
 grid-column-end: 6;
@@ -146,7 +142,6 @@ font-size: 20px;
 font-weight: bold;
 border-bottom: 2px solid black;
 `
-
 const Players = styled.div `
 grid-column-start: 2;
 grid-column-end: 3;
@@ -154,13 +149,11 @@ grid-row-start: 2;
 grid-row-end: 3;
 margin-top: 50px;
 `
-
 const Player = styled.div `
 display: flex;
 justify-content: space-between;
 align-items: center;
 `
-
 const PlayerName = styled.p `
 font-size: 20px;
 font-weight: bold;
@@ -187,22 +180,16 @@ justify-content: flex-end;
 line-height: 20px;
 font-size: 17px;
 `
-
 const MatchInfoHeading = styled.h2`
 margin-top: 0;
 `
-
 const List = styled.ul `
    margin: 0;
    list-style: none;
    padding: 0;
 `
-
-const ListItem = styled.li `
-
-`
+const ListItem = styled.li ``
 const ReadyBtn= styled.button `
-
 grid-column-start: 2;
 grid-column-end: 3;
 grid-row-start: 3;
