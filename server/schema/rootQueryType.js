1ef5b77acc13b87561f15f10f7c8b2ec6e8fbfca
aidamorinashala <aidamshala@gmail.com>
2019-01-09 12:18:49 +0100

    add new queries for creators and opponents

--- server/schema/rootQueryType.js
+++ server/schema/rootQueryType.js
@@ -2,12 +2,46 @@ const axios = require('axios');
 const { GraphQLObjectType, GraphQLInt, GraphQLList } = require('graphql');
 const GameType = require('./types/GameType');
 const PlayerType = require('./types/PlayerType');
+const CreatorType = require('./types/CreatorType');
+const OpponentType = require('./types/OpponentType');
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      getGame: {
-          type: GameType,
+  name: 'RootQueryType',
+  fields: {
+    getCreator: {
+      type: CreatorType,
+      args: { id: { type: GraphQLInt } },
+      resolve(parentValue, args) {
+        console.log('getcreator',parentValue, args)
+        return axios.get(`http://localhost:1337/creators/${args.id}`)
+          .then(resp => {
+            return resp.data});
+      }
+    },
+    getCreators: {
+      type: new GraphQLList(CreatorType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:1337/creators`)
+          .then(resp => resp.data);
+      }
+    },
+    getOpponent: {
+      type: OpponentType,
+      args: { id: { type: GraphQLInt } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:1337/opponents/${args.id}`)
+          .then(resp => resp.data);
+      }
+    },
+    getOpponents: {
+      type: new GraphQLList(OpponentType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:1337/opponents`)
+          .then(resp => resp.data);
+      }
+    },
+    getGame: {
+      type: GameType,
           args: { id: { type: GraphQLInt } },
           resolve(parentValue, args) {
             return axios.get(`http://localhost:1337/games/${args.id}`)
