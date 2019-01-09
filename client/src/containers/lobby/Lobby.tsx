f1542ad2bb51a1f9c90e4b1ac37c161e8b200650
Matilda Hermansson <hermanssonmatilda@gmail.com>
2019-01-09 09:58:36 +0100

    Added NavLink. Now sends opponent to correct gameId await page.

--- client/src/containers/lobby/Lobby.tsx
+++ client/src/containers/lobby/Lobby.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import LobbyCreateGameComponent from 'src/components/lobby/LobbyCreateGame';
 import LobbyFilterComponent from 'src/components/lobby/LobbyFilter';
 import LobbySeeksComponent from 'src/components/lobby/LobbySeeks';
