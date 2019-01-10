66eefd6e44d479a62da796f9bc392742e1e81bdc
aidamorinashala <aidamshala@gmail.com>
2019-01-10 13:26:38 +0100

    getting the apiUrl through config file

--- server/schema/rootQueryType.js
+++ server/schema/rootQueryType.js
@@ -1,10 +1,9 @@
 const axios = require('axios');
+const dbConfig = require('./../config')
 const { GraphQLObjectType, GraphQLString, GraphQLList } = require('graphql');
 const GameType = require('./types/GameType');
-const PlayerType = require('./types/PlayerType');
-const CreatorType = require('./types/CreatorType');
-const OpponentType = require('./types/OpponentType');
 
+const apiUrl = dbConfig.dbPath;
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -12,29 +11,14 @@ const RootQuery = new GraphQLObjectType({
      type: GameType,
      args: { id: { type: GraphQLString } },
           resolve(parentValue, args) {
-            return axios.get(`http://localhost:1337/games/${args.id}`)
+            return axios.get(`${apiUrl}/games/${args.id}`)
               .then(resp => resp.data);
           }
         },
       getGames: {
         type: new GraphQLList(GameType),
         resolve(parentValue) {
-          return axios.get(`http://localhost:1337/games`)
-            .then(resp => resp.data);
-        }
-      },
-      getPlayer: {
-        type: PlayerType,
-        args: { id: { type: GraphQLString } },
-        resolve(parentValue, args) {
-          return axios.get(`http://localhost:1337/players/${args.id}`)
-            .then(resp => resp.data);
-        }
-      },
-      getPlayers: {
-        type: new GraphQLList(PlayerType),
-        resolve(parentValue) {
-          return axios.get(`http://localhost:1337/players`)
+          return axios.get(`${apiUrl}/games`)
             .then(resp => resp.data);
         }
       },
