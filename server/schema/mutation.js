399ed5b9d4d4331ba94d071be0b853ccd1abe1cd
Malin Hanak <malinehanak@gmail.com>
2019-01-11 21:51:47 +0100

    Added mutation for new move

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLNonNull, GraphQLList } = require('graphql');
 const GameType = require('./types/GameType');
-const HistoryType = require('./types/HistoryType');
+const HistoryInput = require('./types/HistoryInput')
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -20,14 +20,18 @@ const mutation = new GraphQLObjectType({
     newMove: {
       type: GameType,
       args: { 
-        id: { type: new GraphQLNonNull(GraphQLString) },
-        fen: { type: GraphQLString },
-        color: { type: GraphQLString }
-        
+        id: { type: GraphQLNonNull(GraphQLString) },
+        history: { type: new GraphQLList(HistoryInput) }
       },
       resolve(parentValue, args) {
-        return axios.patch(`http://localhost:1337/games/${args.id}`, args)
-          .then(res => res.data)
+        return axios.get(`http://localhost:1337/games/${args.id}`)
+          .then((game) => {
+            let history = game.data.history;
+            history.push(args.history[0])
+            args.history = history;
+            return axios.put(`http://localhost:1337/games/${args.id}`, args)
+              .then(res => res.data)
+        })
       }
     }
   }
