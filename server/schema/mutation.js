d3a6bb5ba423c5f30f1993e082572eadebcab34a
aidamorinashala <aidamshala@gmail.com>
2019-01-11 11:26:31 +0100

    addMessage mutation

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLNonNull } = require('graphql');
 const GameType = require('./types/GameType');
+const MessageType = require('./types/MessageType');
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -10,6 +11,17 @@ const mutation = new GraphQLObjectType({
       args: { 
         id: { type: new GraphQLNonNull(GraphQLString) },
         gameStarted: { type: GraphQLBoolean }
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:1337/games/${args.id}`, args)
+          .then(res => res.data)
+      }
+    },
+    addMessage: {
+      type: MessageType,
+      args: { 
+        message: { type: new GraphQLNonNull(GraphQLString) },
+        user: { type: new GraphQLNonNull(GraphQLString) }
       },
       resolve(parentValue, args) {
         return axios.patch(`http://localhost:1337/games/${args.id}`, args)
