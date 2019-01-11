26bb78daef85fddc3fdc6b6893d9f55348cd458e
Malin Hanak <malinehanak@gmail.com>
2019-01-11 13:16:41 +0100

    first try at mutatation

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
-const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLNonNull } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLNonNull, GraphQLList } = require('graphql');
 const GameType = require('./types/GameType');
+const HistoryType = require('./types/HistoryType');
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -10,6 +11,19 @@ const mutation = new GraphQLObjectType({
       args: { 
         id: { type: new GraphQLNonNull(GraphQLString) },
         gameStarted: { type: GraphQLBoolean }
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:1337/games/${args.id}`, args)
+          .then(res => res.data)
+      }
+    },
+    newMove: {
+      type: GameType,
+      args: { 
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        fen: { type: GraphQLString },
+        color: { type: GraphQLString }
+        
       },
       resolve(parentValue, args) {
         return axios.patch(`http://localhost:1337/games/${args.id}`, args)
