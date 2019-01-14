1721d137edfdcce82eb127c08fdc768648346c68
Linus Plume <plumee_91@hotmail.com>
2019-01-14 13:33:40 +0100

    logic for game

--- client/src/store/GameStore.ts
+++ client/src/store/GameStore.ts
@@ -1,13 +1,18 @@
+// tslint:disable:no-console
+// tslint:disable:no-var-requires
 import {BehaviorSubject} from 'rxjs';
-// const Chess = require('chess.js').Chess;
+const Chess = require('chess.js');
 
 const defaultState = {
-  message: 'test',
+  fen: '',
+  history: null,
+  checkmate: false
 };
 
 const subject = new BehaviorSubject(defaultState);
 
 class GameStore {
+  public chess = new Chess()
     constructor() {
         this.setState({})
     }
@@ -24,7 +29,27 @@ class GameStore {
     public updateDemoMessage(payload:any) {
       this.setState(payload)
     }
-
+    public move = (move: any) => {
+      this.chess.move(move)
+      if(this.chess.in_checkmate()) {
+          this.chess.game_over()
+          this.setState({
+            fen: this.chess.fen(),
+            checkmate: true
+          })
+      } else if(this.chess.in_draw()) {
+          this.chess.game_over()
+      } else if(this.chess.in_stalemate()) {
+          this.chess.game_over()
+      } else if(this.chess.insufficient_material()) {
+          this.chess.game_over()
+      } else {
+        this.setState({
+          fen: this.chess.fen(),
+          history: this.chess.history({ verbose: true })
+        })
+      }
+    }
 }
 
 export default new GameStore();
