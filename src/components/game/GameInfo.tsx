95de14eaa0e2a51e1a2a5dc551e874f0b1db1318
Malin Hanak <malinehanak@gmail.com>
2019-01-16 10:01:21 +0100

    Test

--- src/components/game/GameInfo.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-class GameInfoComponent extends React.Component<any, any> {
-  public render() {
-    return "GameInfoComponent"
-  }
-}
-
-export default GameInfoComponent;
--- src/components/game/interfacesGame.d.ts
+++
@@ -1,3 +0,0 @@
-export interface IGameChatHeader {
-    title: string,
-}
